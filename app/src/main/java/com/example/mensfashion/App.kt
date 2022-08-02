41b6ccef7f7564963c9b1364f2a068fc3cca5f79
Attia ali <aaadddccc2005@gmail.com>
2022-08-02 04:41:47 +0300

    - Adds internet permission - Adds hilt and timber to App file - Adds some needed drawables and fonts

--- app/src/main/java/com/example/mensfashion/App.kt
+++ app/src/main/java/com/example/mensfashion/App.kt
@@ -2,15 +2,21 @@ package com.example.mensfashion
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
-
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        initTimber()
     }
 
+    private fun initTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
 
     companion object {
         private var instance: App? = null

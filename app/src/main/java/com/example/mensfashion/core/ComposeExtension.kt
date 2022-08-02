3500968f93bc4822898923961441c2ced4cf44fb
Attia ali <aaadddccc2005@gmail.com>
2022-08-02 04:45:59 +0300

    - Adds theme files for jetpack compose - Adds remember flow that make flow lifecycle aware - Adds UiEvents.kt that contains events that will be between ui and ViewModel

---
+++ app/src/main/java/com/example/mensfashion/core/ComposeExtension.kt
@@ -0,0 +1,17 @@
+package com.example.mensfashion.core
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> rememberFlow(
+    flow: Flow<T>,
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+): Flow<T> {
+    return remember(key1 = flow, key2 = lifecycleOwner) { flow.flowWithLifecycle(lifecycleOwner.lifecycle, Lifecycle.State.STARTED) }
+}

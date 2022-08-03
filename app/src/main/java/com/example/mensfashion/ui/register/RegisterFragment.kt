073939a1b8e0bfdac11bfb9f5a07a42c0431b30f
Aziza Helmy <aziza.helmy35@gmail.com>
2022-08-03 19:13:13 +0200

    initial commit of all screens

---
+++ app/src/main/java/com/example/mensfashion/ui/register/RegisterFragment.kt
@@ -0,0 +1,25 @@
+package com.example.mensfashion.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mensfashion.R
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+}

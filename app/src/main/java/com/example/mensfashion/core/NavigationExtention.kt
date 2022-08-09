dc201db604c48e7bf202d634979e25732852e20a
Ali Tarek Hrhera <alihrhera1@gmail.com>
2022-08-09 08:38:51 +0200

    review result
    please review my comments thx

--- app/src/main/java/com/example/mensfashion/core/NavigationExtention.kt
+++ app/src/main/java/com/example/mensfashion/core/NavigationExtention.kt
@@ -3,6 +3,39 @@ package com.example.mensfashion.core
 import android.os.Bundle
 import androidx.navigation.NavController
 
+/*
+
+/*
+* using default values instead of repeating the code */
+fun NavController.safeNavigation(
+    action: Int? = null,
+    destination: Int? = null,
+    bundle: Bundle? = null,
+    navOptions: NavOptions? = null
+) {
+    when {
+        action != null -> navigate(action, bundle, navOptions)
+        destination != null -> navigate(destination, bundle, navOptions)
+    }
+}
+
+
+fun Fragment.navigateTo(
+    action: Int? = null,
+    destination: Int? = null,
+    bundle: Bundle? = null,
+    navOptions: NavOptions? = null
+) {
+    // using try catch to prevent app crash with fragment creation/navigation  bugs
+    try {
+
+        findNavController().safeNavigation(action, destination, bundle, navOptions)
+    } catch (e: Throwable) {
+        Timber.e(e, "Error-> ")  // debug loger
+    }
+}
+
+*/
 fun NavController.safeNavigation(fragmentResId:Int, action:Int){
     currentDestination?.let {
         if (it.id == fragmentResId){
